00f0da6645217bb341a206fcf66f7ccdaaf23548
Andrew Champion <andrew.champion@gmail.com>
2021-09-11 11:46:19 +0100

    Copy basic widget implementation
    This was yanked from CLEMCLAM circa June 2021, without any attempt to
    refactor or cleanup as a standalone widget, only with unnecessary
    integrations removed.

--- napari_simple_3d_animation/_dock_widget.py
+++ napari_simple_3d_animation/_dock_widget.py
@@ -1,41 +1,156 @@
 """
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the ``napari_experimental_provide_dock_widget`` hook specification.
-see: https://napari.org/docs/dev/plugins/hook_specifications.html
-
-Replace code below according to your needs.
+A napari widget for animating simple camera motions in 3D and exporting movies.
 """
+import math
+import time
+
+import ffmpeg
+from magicgui import magicgui
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
-from magicgui import magic_factory
+import napari
+import numpy as np
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
+class AnimWidget:
+    _worker = None
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+    def __init__(self) -> None:
+        pass
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+    def cancel(self):
+        napari.utils.notifications.show_info("Ending animation")
+        if self._worker is not None:
+            self._worker.quit()
+        self._worker = None
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+    def camera_animation_widget(self):
+        angle_widget = {"widget_type": "Slider", "min": -360, "max": 360}
+
+        MOTION = {
+            "sin": lambda p: math.sin(p * 2.0 * math.pi),
+            "lerp": lambda p: p,
+            "sigmoid": lambda p: expit(p * 10.0 - 10.0),
+        }
+
+        @magicgui(
+            call_button="Toggle animation",
+            x_deg={"value": 22.5, **angle_widget},
+            y_deg=angle_widget,
+            z_deg=angle_widget,
+            motion={"choices": MOTION.keys()},
+            target_fps={"min": 1, "max": 120},
+            repetitions={"widget_type": "FloatSpinBox", "max": float("inf")},
+        )
+        def _widget(
+            viewer: napari.Viewer,
+            x_deg,
+            y_deg,
+            z_deg,
+            motion,
+            duration=10.0,
+            repetitions=float("inf"),
+            target_fps=60.0,
+            record_movie=False,
+            gui_in_movie=False,
+            movie_file="animation.mp4",
+        ):
+            from napari.qt.threading import GeneratorWorker
+
+            if self._worker is not None:
+                self.cancel()
+                return
+
+            worker = GeneratorWorker(
+                anim_worker,
+                viewer.camera.angles,
+                x_deg=x_deg,
+                y_deg=y_deg,
+                z_deg=z_deg,
+                duration=duration,
+                repetitions=repetitions,
+                interval=1.0 / target_fps,
+                f=MOTION[motion],
+            )
+            self._worker = worker
+            worker.yielded.connect(lambda x: setattr(viewer.camera, "angles", x))
+            orig_angles = viewer.camera.angles
+            reset_angles = lambda: setattr(viewer.camera, "angles", orig_angles)
+            worker.aborted.connect(reset_angles)
+            worker.finished.connect(reset_angles)
+            worker.finished.connect(self.cancel)
+
+            if record_movie:
+                f = viewer.screenshot(canvas_only=not gui_in_movie)
+                width = (f.shape[1] // 2) * 2
+                height = (f.shape[0] // 2) * 2
+                writer = MovieWriter(width, height, target_fps, movie_file)
+
+                def write_frame():
+                    self._worker.pause()
+                    frame = viewer.screenshot(canvas_only=not gui_in_movie)
+                    writer.write_frame(frame)
+                    self._worker.resume()
+
+                writer.write_frame(f)
+                worker.yielded.connect(lambda _: write_frame())
+                worker.aborted.connect(writer.finish)
+                worker.finished.connect(writer.finish)
+
+            worker.start()
+
+        return _widget
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def anim_worker(
+    angles, x_deg, y_deg, z_deg, duration, repetitions, interval=0.016, f=math.sin
+):
+    from napari._vispy.quaternion import quaternion2euler
+    from vispy.util.quaternion import Quaternion
+
+    p = 0.0
+
+    orig_q = Quaternion.create_from_euler_angles(*angles, degrees=True)
+    while p < repetitions:
+        p += interval / duration
+        x1 = f(p)
+        a = [x_deg * x1, y_deg * x1, z_deg * x1]
+        a = Quaternion.create_from_euler_angles(*a, degrees=True)
+        a = orig_q * a
+        yield (quaternion2euler(a, degrees=True))
+        time.sleep(interval)
+
+
+class MovieWriter:
+    def __init__(self, width, height, fps, filename, vcodec="libx264") -> None:
+        self.width = width
+        self.height = height
+        self._process = (
+            ffmpeg.input(
+                "pipe:",
+                format="rawvideo",
+                pix_fmt="rgb24",
+                s="{}x{}".format(width, height),
+                r=fps,
+            )
+            .output(filename, pix_fmt="yuv420p", vcodec=vcodec, r=fps)
+            .overwrite_output()
+            .run_async(pipe_stdin=True)
+        )
+
+    def write_frame(self, frame):
+        frame = frame[0 : self.height, 0 : self.width, 0:3]
+        self._process.stdin.write(frame.astype(np.uint8).tobytes())
+
+    def finish(self):
+        self._process.stdin.close()
+        self._process.wait()
+
+
+def expit(x):
+    return 1.0 / (1.0 + math.exp(-x))
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    # you can return either a single widget, or a sequence of widgets
-    return [ExampleQWidget, example_magic_widget]
+    widget = AnimWidget()
+    return widget.camera_animation_widget
